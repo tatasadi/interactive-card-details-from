43fc3283e499528fc9ada35f7200c6e6193f7a76
Ehsan Tatasadi <ehsan@tatasadi.com>
2024-02-14 13:02:08 +0100

    add thank you page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
 "use client"
 import Image from "next/image"
 import cardLogo from "@/public/images/card-logo.svg"
+import iconComplete from "@/public/images/icon-complete.svg"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { SubmitHandler, useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { z } from "zod"
 
 const schema = z.object({
-  cardHolder: z.string().min(1, { message: "Can’t be blank" }),
-  cardNumber: z
+  holder: z.string().min(1, { message: "Can’t be blank" }),
+  number: z
     .string()
     .min(1, { message: "Can’t be blank" })
     .regex(/^\d{4}\d{4}\d{4}\d{4}$/, {
@@ -39,19 +40,44 @@ const schema = z.object({
 
 type Schema = z.infer<typeof schema>
 
+const initialLabels = {
+  number: "0000 0000 0000 0000",
+  holder: "JANE APPLESEED",
+  expirationMonth: "00",
+  expirationYear: "00",
+  cvv: "000",
+}
+
+const initialValues = {
+  holder: "",
+  number: "",
+  expirationMonth: "",
+  expirationYear: "",
+  cvv: "",
+}
+
 export default function Home() {
-  const [cardNumberLabel, setCardNumberLabel] = useState("0000 0000 0000 0000")
-  const [cardHolderLabel, setCardHolderLabel] = useState("JANE APPLESEED")
-  const [cardExpirationMonthLabel, setCardExpirationMonthLabel] = useState("00")
-  const [cardExpirationYearLabel, setCardExpirationYearLabel] = useState("00")
-  const [cardCVCLabel, setCardCVCLabel] = useState("000")
+  const [labels, setLabels] = useState(initialLabels)
+  const [showForm, setShowForm] = useState(true)
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitSuccessful },
+    reset,
   } = useForm<Schema>({
     resolver: zodResolver(schema),
   })
+
+  useEffect(() => {
+    reset(initialValues)
+  }, [isSubmitSuccessful, reset])
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLabels((prevLabels) => ({
+      ...prevLabels,
+      [e.target.name]: e.target.value,
+    }))
+  }
 
   const handleCardNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     let inputVal = e.target.value.replace(/ /g, "") //remove all the empty spaces in the input
@@ -69,11 +95,17 @@ export default function Home() {
       spacedNumber = splits.join(" ") // Join all the splits with an empty space
     }
 
-    setCardNumberLabel(spacedNumber)
+    setLabels((prevLabels) => ({ ...prevLabels, number: spacedNumber }))
   }
 
   const onSubmit: SubmitHandler<Schema> = (data) => {
     console.log(data)
+    setShowForm(false)
+  }
+
+  const handleReset = () => {
+    setLabels(initialLabels)
+    setShowForm(true)
   }
 
   return (
@@ -83,7 +115,7 @@ export default function Home() {
         <div className="relative -mb-[2.66rem] min-h-[15.35rem] w-[21.375rem]">
           <div className="absolute right-0 top-0 h-[9.8125rem] w-[17.875rem] rounded-md bg-[url('/images/bg-card-back.png')] bg-contain bg-no-repeat">
             <p className="label-on-card-small absolute right-[2rem] top-[4.5rem] text-white">
-              {cardCVCLabel}
+              {labels.cvv}
             </p>
           </div>
           <div className="absolute bottom-0 left-0 h-[9.8125rem] w-[17.875rem] rounded-md bg-[url('/images/bg-card-front.png')] bg-contain bg-no-repeat p-5 text-white">
@@ -95,95 +127,112 @@ export default function Home() {
               height={30}
             />
             <p className="mt-[2.31rem] text-lg font-medium tracking-[0.1375rem]">
-              {cardNumberLabel}
+              {labels.number}
             </p>
             <div className="label-on-card-small mt-3 flex uppercase">
-              <p className="">{cardHolderLabel}</p>
+              <p className="">{labels.holder}</p>
               <p className="ml-auto">
-                {cardExpirationMonthLabel} / {cardExpirationYearLabel}
+                {labels.expirationMonth} / {labels.expirationYear}
               </p>
             </div>
           </div>
         </div>
       </div>
       <div className="px-6 pb-[2.81rem] pt-[5.69rem]">
-        <form
-          className="grid grid-cols-2 gap-x-[0.69rem] gap-y-5"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <div className="col-span-2">
-            <Label htmlFor="cardHolder">Cardholder Name</Label>
-            <Input
-              id="cardHolder"
-              type="text"
-              placeholder="e.g. Jane Appleseed"
-              {...register("cardHolder")}
-              onChange={(e) => setCardHolderLabel(e.target.value)}
-              className={errors.cardHolder ? "border-red" : ""}
-            />
-            {errors.cardHolder && (
-              <p className="error-message">{errors.cardHolder.message}</p>
-            )}
-          </div>
-          <div className="col-span-2">
-            <Label htmlFor="cardNumber">Card Number</Label>
-            <Input
-              id="cardNumber"
-              type="text"
-              placeholder="e.g. 1234 5678 9123 0000"
-              {...register("cardNumber")}
-              onChange={handleCardNumberChange}
-              className={errors.cardNumber ? "border-red" : ""}
-            />
-            {errors.cardNumber && (
-              <p className="error-message">{errors.cardNumber.message}</p>
-            )}
-          </div>
-          <div className="col-span-1">
-            <Label htmlFor="expirationMonth">Exp. Date (MM/YY)</Label>
-            <div className="flex gap-2">
+        {showForm ? (
+          <form
+            className="grid grid-cols-2 gap-x-[0.69rem] gap-y-5"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <div className="col-span-2">
+              <Label htmlFor="holder">Cardholder Name</Label>
               <Input
-                id="expirationMonth"
+                id="holder"
                 type="text"
-                placeholder="MM"
-                {...register("expirationMonth")}
-                onChange={(e) => setCardExpirationMonthLabel(e.target.value)}
-                className={errors.expirationMonth ? "border-red" : ""}
+                placeholder="e.g. Jane Appleseed"
+                {...register("holder")}
+                onChange={handleChange}
+                className={errors.holder ? "border-red" : ""}
               />
-              <Input
-                id="expirationYear"
-                type="text"
-                placeholder="YY"
-                {...register("expirationYear")}
-                onChange={(e) => setCardExpirationYearLabel(e.target.value)}
-                className={errors.expirationYear ? "border-red" : ""}
-              />
+              {errors.holder && (
+                <p className="error-message">{errors.holder.message}</p>
+              )}
             </div>
-            {errors.expirationMonth && (
-              <p className="error-message">{errors.expirationMonth.message}</p>
-            )}
-            {!errors.expirationMonth && errors.expirationYear && (
-              <p className="error-message">{errors.expirationYear.message}</p>
-            )}
+            <div className="col-span-2">
+              <Label htmlFor="number">Card Number</Label>
+              <Input
+                id="number"
+                type="text"
+                placeholder="e.g. 1234 5678 9123 0000"
+                {...register("number")}
+                onChange={handleCardNumberChange}
+                className={errors.number ? "border-red" : ""}
+              />
+              {errors.number && (
+                <p className="error-message">{errors.number.message}</p>
+              )}
+            </div>
+            <div className="col-span-1">
+              <Label htmlFor="expirationMonth">Exp. Date (MM/YY)</Label>
+              <div className="flex gap-2">
+                <Input
+                  id="expirationMonth"
+                  type="text"
+                  placeholder="MM"
+                  {...register("expirationMonth")}
+                  onChange={handleChange}
+                  className={errors.expirationMonth ? "border-red" : ""}
+                />
+                <Input
+                  id="expirationYear"
+                  type="text"
+                  placeholder="YY"
+                  {...register("expirationYear")}
+                  onChange={handleChange}
+                  className={errors.expirationYear ? "border-red" : ""}
+                />
+              </div>
+              {errors.expirationMonth && (
+                <p className="error-message">
+                  {errors.expirationMonth.message}
+                </p>
+              )}
+              {!errors.expirationMonth && errors.expirationYear && (
+                <p className="error-message">{errors.expirationYear.message}</p>
+              )}
+            </div>
+            <div className="col-span-1">
+              <Label htmlFor="cvv">CVV</Label>
+              <Input
+                id="cvv"
+                type="text"
+                placeholder="e.g. 123"
+                {...register("cvv")}
+                onChange={handleChange}
+                className={errors.cvv ? "border-red" : ""}
+              />
+              {errors.cvv && (
+                <p className="error-message">{errors.cvv.message}</p>
+              )}
+            </div>
+            <Button className="col-span-2 mt-2" type="submit">
+              Confirm
+            </Button>
+          </form>
+        ) : (
+          <div className="flex flex-col items-center font-medium">
+            <Image src={iconComplete} alt="Icon Complete" />
+            <h2 className="text-very-dark-violet mt-[2.19rem] text-[1.75rem] uppercase leading-normal tracking-[0.21388rem]">
+              Thank you!
+            </h2>
+            <p className="text-dark-grayish-violet mt-4 text-lg">
+              We’ve added your card details
+            </p>
+            <Button className="mt-12 w-full" onClick={handleReset}>
+              Continue
+            </Button>
           </div>
-          <div className="col-span-1">
-            <Label htmlFor="cvv">CVV</Label>
-            <Input
-              id="cvv"
-              type="text"
-              placeholder="e.g. 123"
-              {...register("cvv")}
-              onChange={(e) => setCardCVCLabel(e.target.value)}
-              className={errors.cvv ? "border-red" : ""}
-            />
-            {errors.cvv && (
-              <p className="error-message">{errors.cvv.message}</p>
-            )}
-          </div>
-          <Button className="col-span-2 mt-2" type="submit">
-            Confirm
-          </Button>
-        </form>
+        )}
       </div>
     </main>
   )
